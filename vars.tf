3127f708bf56d6e4acee4534277e77b9969050b0
Ronald Konjer <ronaldkonjer@gmail.com>
2018-02-19 23:56:10 +0100

    Initial commit

---
+++ vars.tf
@@ -0,0 +1,37 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "ECS_INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+variable "ECS_AMIS" {
+  type = "map"
+  default = {
+    eu-west-1 = "ami-880d64f1"
+    eu-west-2 = "ami-42a64325"
+    eu-west-3 = "ami-6fa21412"
+  }
+}
+# Full List: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
+
+variable "AMIS" {
+  type = "map"
+  default = {
+    eu-west-1 = "ami-c1167eb8"
+    eu-west-2 = "ami-e0bc5987"
+    eu-east-3 = "ami-6bad1b16"
+  }
+}
+variable "INSTANCE_DEVICE_NAME" {
+  default = "/dev/xvdh"
+}
+variable "JENKINS_VERSION" {
+  default = "2.32.1"
+}
+
